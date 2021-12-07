ed891e6454537b3a2c4eb2139d11324d3a85e169
pameliapek <pekxuan@hotmail.com>
2021-12-07 19:18:18 +0800

    add file

--- app/src/main/java/com/example/pickafood/LogInActivity.java
+++ app/src/main/java/com/example/pickafood/LogInActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FilterActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_filter);
+        setContentView(R.layout.activity_log_in);
     }
 }
